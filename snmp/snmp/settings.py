08e9902e7f9988f7b197f310d505f963422d0f4a
selvasachein <selvakumarr@saveetha.ac.in>
2023-09-16 11:27:52 +0530

    Success

--- snmp/snmp/settings.py
+++ snmp/snmp/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
-
+import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-d8c&jy=lb*9j-el^2fngdfi)c0*q^!vn$+xec!84cj%)l01jm8
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -116,7 +116,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
-
+STATICFILES_DIRS=[
+os.path.join(BASE_DIR,'myapp/static')
+]
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
